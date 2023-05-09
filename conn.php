ea94eea216d9819793b7c4d73648a37c081f17ff
Iago Melo <iagomello160@gmail.com>
2023-05-09 10:14:35 -0300

    Update conn.php
    Review of the code

--- conn.php
+++ conn.php
@@ -1,10 +1,6 @@
 <?php
 
-$conn = new mysqli("162.241.2.204", "divasp33_master", "qRfWMk6VYqJyW8B", "divasp33_store");
-
-// $conn = new mysqli("localhost", "root", "", "store");
-
-// $conn = new mysqli("localhost", "divasp33_master", "qRfWMk6VYqJyW8B", "divasp33_store");
+$conn = new mysqli("localhost", "root", "", "store");
 
 mysqli_set_charset($conn, 'utf8mb4');
 
