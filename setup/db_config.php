37883d272410433b1c67b4740753d1a2273027d3
Iago Melo <iagomello160@gmail.com>
2023-09-20 16:02:57 -0300

    Update db_config.php

--- setup/db_config.php
+++ setup/db_config.php
@@ -3,7 +3,7 @@
 define('DB_HOST', 'seu_host'); // Endereço do servidor do banco de dados
 define('DB_USER', 'seu_usuario'); // Nome de usuário do banco de dados
 define('DB_PASS', 'sua_senha'); // Senha do banco de dados
-define('DB_NAME', 'nome_do_banco'); // Nome do banco de dados
+define('DB_NAME', 'nome_do_banco'); // Nome do banco de dados nome recomendado = 'store'
 
 // Outras configurações
 define('DB_CHARSET', 'utf8'); // Conjunto de caracteres (RECOMENDADO utf8)
